7245e1bada8e4f4dddcb44d9b4238ec75f534c3d
24-37521 <24-37521@g.batstate-u.edu.ph>
2025-11-07 15:05:10 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -9,6 +9,18 @@ CREATE TABLE Owners (
     email VARCHAR(100)
 );
 
+CREATE TABLE animals (
+    animalid INT PRIMARY KEY,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    dateofbirth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+    ownerid INT,
+    FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
+
 CREATE TABLE appointments (
     appointid INT PRIMARY KEY,
     animalid INT,
