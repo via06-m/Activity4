f88c49455580cd14ca21767e974eb074d547c207
via06-m <24-32265@gmail.com>
2025-11-07 11:58:21 +0800

    Create Owners table with relevant fields

---
+++ veterinary.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Owners (
+    ownerid INT PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
